b9e911c775d327e06e20298ce6bcd94cb04334e4
urielaero <aero.uriel@gmail.com>
2014-11-29 02:19:07 -0600

    asm v1

--- parser.py
+++ parser.py
@@ -37,19 +37,30 @@ class Token(object):
         #uno antes para indicar
         token = self.tokens[self.index-1]
         token.infoError("se esperaba "+debe+" se encontro "+self.token.tipo)
-	exit(1)
+        exit(1)
 
     def objeto(self):
         return self.token
 
 
 class Parser(object):
+
+    varId = []
+    const = []
+    etiquetasDict = {
+        'etiquetaFinalActual':[],
+        'etiquetaInicioActual':[],
+        'while':0,
+        'if':0
+    }
+
     def __init__(self,tokens,nodo = False):
         self.token = tokens
         if nodo != False:
             self.nodo = nodo
         else:
             self.nodo = []
+
 
     def parse(self):
         p = Program(self.token)
@@ -70,7 +81,58 @@ class Parser(object):
             print i.nombre()
             if hasattr(i,'imprimir'):
                 i.imprimir(indent+1)
+    
+    #def traducir(self,indice=0):
+    #    return self.traducirNodo(indice)
 
+    def traducirNodos(self,indice):
+        for i in self.nodo:
+            if hasattr(i,'traducir'):
+                i.traducir(indice)
+    
+    def getLevelVarId(self,indice,name):
+        last = False
+        mejor = self.varId[0]
+        for i in self.varId:
+            tId = i.get("token",False)
+            lId = i.get("nivel")
+            if tId and tId == name and lId <= indice:
+                if not last or i["nivel"] >= last["nivel"]:
+                    last = i
+        var = True
+        if not last:
+            var = False
+            for i in self.const:
+                tId = i.get(name,False)
+                lId = i.get("nivel")
+                if tId!=False and lId <= indice:
+                    if not last or i["nivel"] >= last["nivel"]:
+                        last = i
+            return tId,False
+                
+        nivel = indice - last["nivel"]
+        return nivel,var
+        #if not last ERROR no id
+
+    def getEtiquetaFinal(self):
+        ets = self.etiquetasDict['etiquetaFinalActual']
+        if len(ets):
+            et = ets.pop()
+            #,et[:-1]
+            if et[-1] == ':':
+                return et[:-1]
+            return et
+        #error
+
+    def getEtiquetaInicio(self):
+        ets = self.etiquetasDict['etiquetaInicioActual']
+        if len(ets):
+            et = ets.pop()
+            print "sadasda!!!!!!!!!!!!!!!!!!!!"
+            if et[-1] == ':':
+                return et[:-1]
+            return et
+        #error
 
 class Program(Parser):
     def parse(self):
@@ -83,6 +145,10 @@ class Program(Parser):
             return True
         else:
             self.token.makeError('"." al final')
+
+    def traducir(self,indice):
+        print "Program ",indice
+        return super(Program,self).traducirNodos(indice)
 
 class Bloque(Parser):
     def parse(self):
@@ -116,14 +182,19 @@ class Bloque(Parser):
         
         if res:
             #self.token.permitirSiguiente()
-            instruccion = Instruccion(self.token)
+            instruccion = Instruccion(self.token,self.nodo)
             res = instruccion.parse()
             if res:
-                self.nodo.append(instruccion)
+	    	pass
+                #self.nodo.append(instruccion)
         else:
             print "mal antes statement!",t
             exit(1)
         return res
+
+    def traducir(self,indice):
+        print "Bloque ",indice
+        return super(Bloque,self).traducirNodos(indice+1)
 
 class SeccionConst(Parser):
     def parse(self):
@@ -159,6 +230,23 @@ class SeccionConst(Parser):
         else:
             self.token.makeError("[SeccionConst]identificador")
         return False
+
+    def traducir(self,indice):
+        print "Const ",indice
+        #print dir(self)
+        dic = {}
+        
+        for i,nodo in enumerate(self.nodo):
+            val = nodo.token;
+            if i%2 == 0:
+                back = val
+            else:
+                dic[back] = val
+                
+
+        dic['nivel'] = indice
+        self.const.append(dic)
+        return super(SeccionConst,self).traducirNodos(indice)
             
 class SeccionVar(Parser):
     def parse(self):
@@ -179,6 +267,14 @@ class SeccionVar(Parser):
         else:
             self.token.makeError('[SeccionVar] identificador')
         return False
+
+    def traducir(self,indice):
+        print "Var ",indice
+        print "INS {} {} ;allocate!".format(indice,len(self.nodo)+3)
+        for nodo in self.nodo:
+            self.varId.append({"nivel":indice,"token":nodo.token})
+        
+        return super(SeccionVar,self).traducirNodos(indice)
 
 class SeccionProcedure(Parser):
     def parse(self):
@@ -206,6 +302,13 @@ class SeccionProcedure(Parser):
             self.token.makeError("[SeccionProcedure] identificador")
 
         return False
+
+    def traducir(self,indice):
+        print "Procedure ",indice
+        procedureName = self.nodo[0].token
+        print "INICIO{}:".format(procedureName)
+        super(SeccionProcedure,self).traducirNodos(indice)
+        print "OPR 0,0"
       
 class Instruccion(Parser):
     def parse(self):
@@ -224,17 +327,21 @@ class Instruccion(Parser):
                 return res
 
         elif t == 'token_call':
-            self.nodo.append(self.token.objeto())
+            nodoCall = self.token.objeto()
+            #self.nodo.append(nodoCall)
             self.token.permitirSiguiente()
             t = self.token.siguiente()
             if t == 'token_id':
+                nodoCall.val = self.token.objeto()
+                self.nodo.append(nodoCall)
                 return True
         elif t == 'token_begin':
             seguir = True
             while True:
                 self.token.permitirSiguiente()
-                instruccion = Instruccion(self.token,self.nodo)
+                instruccion = Instruccion(self.token)
                 res = instruccion.parse()
+                self.nodo.append(instruccion)
                 self.token.permitirSiguiente()
                 t = self.token.siguiente()
                 if res and t == 'token_punto_coma':
@@ -254,6 +361,12 @@ class Instruccion(Parser):
                     return False
         
         elif t == 'token_if':
+            iF = If(self.token)
+            res = iF.parse()
+            if res:
+                self.nodo.append(iF)
+                return res
+            """
             condicion = Condicion(self.token)
             if condicion.parse():
                 self.nodo.append(condicion)
@@ -266,22 +379,83 @@ class Instruccion(Parser):
                         return True
                     else:
                         self.token.makeError('[instruccion-if] ')
+            """
         elif t == 'token_while':
             #self.token.permitirSiguiente()
             #t = self.token.siguiente()
-            condicion = Condicion(self.token)
-            if condicion.parse():
-                self.nodo.append(condicion)
-                self.token.permitirSiguiente()
-                t = self.token.siguiente()
-                if t == 'token_do':
-                    self.token.permitirSiguiente()
-                    instruccion = Instruccion(self.token,self.nodo)
-                    if instruccion.parse():
-                        return True
+            w = While(self.token)
+            res = w.parse()
+            if res:
+                self.nodo.append(w)
+                return res
+
         else:
             self.token.makeError('[Instruccion]')
         return False
+
+    def traducir(self,indice):
+        super(Instruccion,self).traducirNodos(indice)
+        for nodo in self.nodo:
+            if hasattr(nodo,"tipo"):
+                tId = nodo.token
+                if nodo.tipo == 'token_id':
+                    identId,var = self.getLevelVarId(indice,nodo.token)
+                    if var:
+                        print "ALM {} {}".format(identId,nodo.token)
+                elif nodo.tipo == 'token_call':
+                    print "LLA 0,INICIO{}".format(nodo.val.token)
+
+class If(Parser):
+    def parse(self):
+        self.nodo.append(self.token.objeto())
+        condicion = Condicion(self.token)
+        if condicion.parse():
+            self.nodo.append(condicion)
+            self.token.permitirSiguiente()
+            t = self.token.siguiente()
+            if t == 'token_then':
+                self.token.permitirSiguiente()
+                instruccion = Instruccion(self.token,self.nodo)
+                if instruccion.parse():
+                    return True
+                else:
+                    self.token.makeError('[instruccion-if] ')
+
+    def traducir(self,indice):
+        #print "While ",indice
+        self.etiquetasDict["if"] += 1
+        ifName = self.etiquetasDict['if']
+        #print "INICIAIF_{}:".format(ifName)
+        ifFinal = "TERMINAIF_{}:".format(ifName)
+        self.etiquetasDict['etiquetaFinalActual'].append(ifFinal)
+        super(If,self).traducirNodos(indice)
+        print ifFinal
+
+class While(Parser):
+    def parse(self):
+        self.nodo.append(self.token.objeto())
+        condicion = Condicion(self.token)
+        if condicion.parse():
+            self.nodo.append(condicion)
+            self.token.permitirSiguiente()
+            t = self.token.siguiente()
+            if t == 'token_do':
+                self.token.permitirSiguiente()
+                instruccion = Instruccion(self.token,self.nodo)
+                if instruccion.parse():
+                    return True
+
+    def traducir(self,indice):
+        #print "While ",indice
+        self.etiquetasDict["while"] += 1
+        whileName = self.etiquetasDict['while']
+        whileInicia = "INICIAWHILE_{}:".format(whileName)
+        print whileInicia
+        whileFinal = "TERMINAWHILE_{}:".format(whileName)
+        self.etiquetasDict['etiquetaFinalActual'].append(whileFinal)
+        super(While,self).traducirNodos(indice)
+        print "SAL 0,",whileInicia[:-1]
+        print whileFinal
 
 class Condicion(Parser):
     def parse(self):
@@ -310,7 +484,8 @@ class Condicion(Parser):
                         'token_menor_igual',
                         'token_mayor_igual'
                     ]:
-                    
+
+                self.nodo.append(self.token.objeto())               
                 expresion = Expresion(self.token)
                 self.nodo.append(expresion)
                 res = expresion.parse()
@@ -318,6 +493,13 @@ class Condicion(Parser):
                 self.token.makeError('= o <> o < o > o <= o !=')
         
         return res
+
+    def traducir(self,indice):
+        super(Condicion,self).traducirNodos(indice)
+        for nodo in self.nodo:
+            if hasattr(nodo,'tipo'):
+                print "OPR 0,",nodo.token
+                print "SAC 0,",self.getEtiquetaFinal()
 
 class Expresion(Parser):
     def parse(self):
@@ -352,6 +534,13 @@ class Expresion(Parser):
             self.token.regresa()
 
         return res
+
+    def traducir(self,indice):
+        #print "Expresion !!!!!!!",indice
+        super(Expresion,self).traducirNodos(indice)
+        for nodo in self.nodo:
+            if hasattr(nodo,'tipo'):
+                print "OPR 0",nodo.token
         
 #TODO quitar este:
 class Expresion2(Parser):
@@ -427,6 +616,13 @@ class Term(Parser):
                 return True
         return res
         """
+    def traducir(self,indice):
+        #print "Term !!!!!!!",indice
+        super(Term,self).traducirNodos(indice)
+        for nodo in self.nodo:
+            if hasattr(nodo,'tipo'):
+                print "OPR 0,",nodo.token
+
 class Factor(Parser):
     def parse(self):
         t = self.token.token.tipo
@@ -450,14 +646,30 @@ class Factor(Parser):
             self.token.makeError('[Factor] identificador o numero o "("')
         return False
 
+    def traducir(self,indice):
+        fac =  self.nodo[0]
+        if fac.tipo == 'token_id':
+            facId,var = self.getLevelVarId(indice,fac.token)
+            if var:
+                print "CAR {},{}".format(facId,fac.token)
+            else:
+                print "LIT 0,{}".format(facId)
+        elif fac.tipo == 'token_numero':
+            print "LIT 0,{}".format(fac.token)
+        super(Factor,self).traducirNodos(indice)
+
+def parse(nombreArchivo):
+    tokens = Token(nombreArchivo)
+    p = Parser(tokens)
+    p.parse()
+    return p
 
 
 if __name__=="__main__":
     if len(sys.argv) == 2:
-        tokens = Token(sys.argv[1])
-        p = Parser(tokens)
-        p.parse()
+        p = parse(sys.argv[1])
         p.imprimir()
+        p.traducirNodos(-1)
     else:
         print "uso: ",sys.argv[0]," nombreArchivo.pl0"
 """
